579d8e4b7b02ab2195917d241f2941a8cd1d5986
kimiasami1378 <64108043+kimiasami1378@users.noreply.github.com>
2025-01-28 00:05:29 -0500

    Add files via upload

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PolicyChat from './components/PolicyChat';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ minHeight: '100vh', backgroundColor: '#f5f5f5' }}>
+      <PolicyChat />
+    </div>
+  );
+};
+
+export default App;
